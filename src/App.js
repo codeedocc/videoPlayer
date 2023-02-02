97c90b91f685f2294d3ba46680730c408f885fb8
Ruslan Sharifullin <newwavecfy@gmail.com>
2023-02-02 21:33:20 +0300

    created route structure for project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-function App() {
-  return (
-    <div>
-      <h1>hello world!</h1>
-    </div>
-  )
-}
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+import {
+  Navbar,
+  ChannelDetail,
+  Feed,
+  SearchFeed,
+  VideoDetail,
+} from './components'
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+)
 
 export default App
