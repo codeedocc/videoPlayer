456ef727d8a974ff8ebc11de8f4e07ee61d02ae5
Ruslan Sharifullin <newwavecfy@gmail.com>
2023-02-05 17:23:02 +0300

    created ChannelCard box

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,8 +11,11 @@ import GraphicEqIcon from '@mui/icons-material/GraphicEq'
 import TheaterComedyIcon from '@mui/icons-material/TheaterComedy'
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter'
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode'
+import youLook from './assets/youLook.png'
+import youLookChannel from './assets/channel-logo.png'
 
-export const logo = 'https://i.ibb.co/s9Qys2j/logo.png'
+export const logo = youLook
+export const logoChannel = youLookChannel
 
 export const categories = [
   { name: 'New', icon: <HomeIcon /> },
