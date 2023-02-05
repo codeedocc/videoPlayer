7d427a591afb940f80a45e8ebe1903b846fbf767
Ruslan Sharifullin <newwavecfy@gmail.com>
2023-02-05 18:59:31 +0300

    added functionality to search different videos

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,11 +5,22 @@ import SearchIcon from '@mui/icons-material/Search'
 
 const SearchBar = () => {
   const navigate = useNavigate()
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const handleSumbit = (e) => {
+    e.preventDefault()
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`)
+
+      setSearchTerm('')
+    }
+  }
 
   return (
     <Paper
       component="form"
-      onSubmit={() => {}}
+      onSubmit={handleSumbit}
       sx={{
         borderRadius: 20,
         border: '1px solid #e3e3e3',
@@ -22,8 +33,8 @@ const SearchBar = () => {
         type="text"
         className="search-bar"
         placeholder="Найти..."
-        value=""
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
       <IconButton type="submit" sx={{ p: '10px', color: 'red' }}>
         <SearchIcon />
