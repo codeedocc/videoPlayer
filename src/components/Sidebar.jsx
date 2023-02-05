952bfab41c85d4ded525e15ffad078558b8fc131
Ruslan Sharifullin <newwavecfy@gmail.com>
2023-02-05 15:10:03 +0300

    created dynamic display for current page in Sidebar and Feed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,9 +3,7 @@ import { Stack } from '@mui/material'
 
 import { categories } from '../utils/constants'
 
-const selectedCategory = 'New'
-
-const Sidebar = () => (
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => (
   <Stack
     direction="row"
     sx={{
@@ -22,6 +20,7 @@ const Sidebar = () => (
           color: 'white',
         }}
         key={category.name}
+        onClick={() => setSelectedCategory(category.name)}
       >
         <span
           style={{
